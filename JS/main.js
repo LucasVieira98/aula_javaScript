a3ce3282748523026e047bf27377b3e1c086d36a
LucasVieira98 <lucasmatheus.adm98@gmail.com>
2019-10-14 21:39:22 -0300

    Exemplos de JavaScript

---
+++ JS/main.js
@@ -0,0 +1,127 @@
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar<b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://google.com");
+    window.location.href = "https://google.com";
+    
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagina Carrega");
+}
+
+function fucaoChange (elemento){
+    console.log(elemento.value);
+}
+*/
+/*
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+function validaIdade(idade){
+    var validar = true;
+    if(idade >= 18){
+        validar = true
+    }
+    else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+*/
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getDay()+1);
+alert(d.getHours()+1);
+alert(d.getMinutes()+1);
+*/
+/*
+var count;
+for (count = 0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+
+
+/*
+var fruta = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(fruta.nome);
+alert(fruta[1].nome);
+*/
+
+/*
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop("laranja");
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" - "))
+
+
+
+
+//var nome = "Lucas vieira";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = ("Japão é o melhor time do mundo");
+/*alert(nome +" tem "+ idade);*/
+//alert(idade + idade2);
+
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
